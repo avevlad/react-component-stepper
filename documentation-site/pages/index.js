ae7e91f7180683c7bae1c30ac64b48505caaa315
avevlad <vlad.open.source@gmail.com>
2020-04-24 22:49:24 +0300

    chore: bump now, last

--- documentation-site/pages/index.js
+++ documentation-site/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next App</title>
+        <title>Next App Work!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
